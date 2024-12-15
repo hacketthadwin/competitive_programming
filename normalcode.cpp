4ddfefb5e32b13ee4c9fca23b9910b84abbe6466
Adarsh <137900871+hacketthadwin@users.noreply.github.com>
2024-12-15 18:54:46 +0530

    normalcode.cpp

--- normalcode.cpp
+++ normalcode.cpp
@@ -1,4 +1,5 @@
 //Adarsh Jha
+//lowest < current < greatest
 #include <bits/stdc++.h>
 using namespace std;
 using namespace chrono;
@@ -9,8 +10,9 @@ typedef pair<string, string> pss;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<pii> vii;
-#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
-#define rep(n) FOR(i, 0, n)
+#define repn(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define rep(n) repn(i, 0, n)
+#define repi(i,n) repn(i, 0, n)
 #define pb push_back
 #define ALL(v) v.begin(), v.end()
 #define ALLA(arr, sz) arr, arr + sz
@@ -19,10 +21,13 @@ typedef vector<pii> vii;
 #define PERMUTE next_permutation
 #define TC(t) while (t--)
 #define mod 1000000007
+#define count_set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
 const int N=1e5+1;
 #define maxa(arr,n) *max_element(arr, arr+n)
 #define mina(arr,n) *min_element(arr, arr+n)
-
+#define pn cout << "NO\n";
+#define py cout << "YES\n";
 int lcm(int a,int b){return ((a/__gcd(a,b))*b);}
 bool isprime(int n){if(n==1||n==0)return false;for(int i=2;i*i<=n;i++){if(n%i==0) return false;}return true;}
 void prime_factor(vector<pii> &factorization,int n){for(int i=2; i*i <= n; i++) {int count = 0;while(n % i == 0) {count++;n /= i;}if(count > 0) {factorization.push_back({i, count});}}if(n != 1) {factorization.push_back({n, 1});}}
@@ -33,17 +38,23 @@ void convertToLowercase(string &str) {for (char &c : str) {if (c >= 'A' && c <= 
 int maximumrepitationofarray(int n,int arr[]){int count=0;int maxcount=1;int c= arr[0];rep(n){if(arr[i]==c){count++;if(count>maxcount){maxcount=count;}}else{count=1;c=arr[i];}}return maxcount;}
 bool poweroftwo(int n) {return !(n & (n - 1));}
 int power(int A, int B) {int res = 1;while (B > 0) {if (B % 2 == 1) {    res *= A;}A *= A;B /= 2;}return res;}
-/*
-REMOVE LEADING ZEROES
+/*REMOVE LEADING ZEROES
 size_t pos = result.find_first_not_of('0');
 result = (pos == std::string::npos) ? "0" : result.substr(pos);
 */
 #define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+void __print(long long x) { cerr << x; }void __print(unsigned x) { cerr << x; }void __print(unsigned long x) { cerr << x; }void __print(unsigned long long x) { cerr << x; }void __print(float x) { cerr << x; }void __print(double x) { cerr << x; }void __print(long double x) { cerr << x; }void __print(char x) { cerr << '\'' << x << '\''; }void __print(const char *x) { cerr << '\"' << x << '\"'; }void __print(const string &x) { cerr << '\"' << x << '\"'; }void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename A>void __print(const A &x);template <typename A, typename B>void __print(const pair<A, B> &p);template <typename... A>void __print(const tuple<A...> &t);template <typename T>void __print(stack<T> s);template <typename T>void __print(queue<T> q);template <typename T, typename... U>void __print(priority_queue<T, U...> q);template <typename A>void __print(const A &x) {    bool first = true;    cerr << '{';    for (const auto &i : x) {        cerr << (first ? "" : ","), __print(i);        first = false;    }    cerr << '}';}template <typename A, typename B>void __print(const pair<A, B> &p) {   cerr << '(';   __print(p.first);   cerr << ',';   __print(p.second);   cerr << ')';}template <typename... A>void __print(const tuple<A...> &t) {  bool first = true;  cerr << '(';    apply([&first](const auto &...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);   cerr << ')';}template <typename T>void __print(stack<T> s) {   vector<T> debugVector;   while (!s.empty()) {        T t = s.top();        debugVector.push_back(t);        s.pop();    }    reverse(debugVector.begin(), debugVector.end());    __print(debugVector);}template <typename T>void __print(queue<T> q) {   vector<T> debugVector;   while (!q.empty()) {       T t = q.front();       debugVector.push_back(t);       q.pop();   }    __print(debugVector);}template <typename T, typename... U>void __print(priority_queue<T, U...> q) {   vector<T> debugVector;   while (!q.empty()) {       T t = q.top();      debugVector.push_back(t);      q.pop();}__print(debugVector);}void _print() { cerr << "]\n"; }template <typename Head, typename... Tail>void _print(const Head &H, const Tail &...T) {__print(H);if (sizeof...(T))    cerr << ", ";_print(T...);}
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 /**************CODE_BEGINS_HERE************************************************/
 
 void solve()
 {   
-
+    
 }
 
 /**************CODE_ENDS_ HERE************************************************/
